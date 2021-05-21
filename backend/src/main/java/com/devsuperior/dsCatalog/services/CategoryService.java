279e871f2adfbb206eff432be44013bccbacd419
Gerson <gersondalto@gmail.com>
2021-05-20 23:00:13 -0300

    put Category

--- backend/src/main/java/com/devsuperior/dsCatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dsCatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dsCatalog.dto.CategoryDTO;
 import com.devsuperior.dsCatalog.entities.Category;
 import com.devsuperior.dsCatalog.repositories.CategoryRepository;
-import com.devsuperior.dsCatalog.services.exception.EntityNotFoundException;
+import com.devsuperior.dsCatalog.services.exception.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -28,16 +30,28 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity Not Found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
 		return new CategoryDTO(entity);
 
 	}
 
 	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
-			Category entity = new Category();
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
 			entity.setName(dto.getName());
 			entity = repository.save(entity);
 			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id Not Found" + id);
+		}
 	}
 }
