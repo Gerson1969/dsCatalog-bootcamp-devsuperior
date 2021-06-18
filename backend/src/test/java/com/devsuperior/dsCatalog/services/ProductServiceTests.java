e94974732a0ac1435028b92ed55f4bc61ce7f26e
Gerson <gersondalto@gmail.com>
2021-06-18 20:09:02 -0300

    Mock and Mockito Tests

---
+++ backend/src/test/java/com/devsuperior/dsCatalog/services/ProductServiceTests.java
@@ -0,0 +1,108 @@
+package com.devsuperior.dsCatalog.services;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsCatalog.dto.ProductDTO;
+import com.devsuperior.dsCatalog.entities.Product;
+import com.devsuperior.dsCatalog.repositories.ProductRepository;
+import com.devsuperior.dsCatalog.services.exception.DataBaseException;
+import com.devsuperior.dsCatalog.services.exception.ResourceNotFoundException;
+import com.devsuperior.dsCatalog.tests.Factory;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+
+	@InjectMocks
+	private ProductService service;
+
+	@Mock
+	private ProductRepository repository;
+
+	private long existingId;
+	private long nonExistingId;
+	private long dependentId;
+	private PageImpl<Product> page;
+	private Product product;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 2L;
+		dependentId = 3L;
+		product = Factory.createProduct();
+		page = new PageImpl<>(List.of(product));
+		
+		when(repository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
+		when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+		when(repository.findById(existingId)).thenReturn(Optional.of(product));
+		when(repository.findById(nonExistingId)).thenReturn(Optional.empty());		
+		
+		doNothing().when(repository).deleteById(existingId);
+		doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnPage() {
+		Pageable pageable = PageRequest.of(0, 10);
+		Page<ProductDTO> result = service.findAllPaged(pageable);
+		
+		Assertions.assertNotNull(result);
+		verify(repository).findAll(pageable);
+		
+	}
+	
+	@Test
+	public void deleteShouldThrowDataBaseExceptionWhenDependentId() {
+		Assertions.assertThrows(DataBaseException.class, () -> {
+			service.delete(dependentId);
+		});
+
+		verify(repository, times(1)).deleteById(dependentId);
+
+	}
+	
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdNotExists() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+
+		verify(repository, times(1)).deleteById(nonExistingId);
+
+	}
+	
+	@Test
+	public void deleteShouldDoNothingWhenIdExists() {
+		Assertions.assertDoesNotThrow(() -> {
+			service.delete(existingId);
+		});
+
+		verify(repository, times(1)).deleteById(existingId);
+
+	}
+
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsCatalog/tests/Factory.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dsCatalog.tests;
+
+import java.time.Instant;
+
+import com.devsuperior.dsCatalog.dto.ProductDTO;
+import com.devsuperior.dsCatalog.entities.Category;
+import com.devsuperior.dsCatalog.entities.Product;
+
+public class Factory {
+	
+	public static Product createProduct() {
+		Product product = new Product(1L, "Phone", "GoodPhone", 800.0, "http://img.com/imp.png", Instant.parse("2021-06-18T18:36:00Z"));
+		product.getCategories().add(new Category(2L,"Electronics"));
+		return product;
+	}
+
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+}
